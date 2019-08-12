b539354953414894ad46807ec4b61a6fcb098bce
Otto Hiltunen <2268682h@student.gla.ac.uk>
2019-08-12 20:16:01 +0300

    graphql crud operations done

--- index.js
+++ index.js
@@ -1,39 +1,18 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
-const Post = require('./models/Post');
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers/index')
 const { MONGODB } = require('./config');
 
-const typeDefs = gql`
-	type Post {
-		id: ID!
-		body: String!
-		createdAt: String!
-		username: String!
-	}
-
-	type Query {
-		getPosts: [Post]
-	}
-`;
-
-const resolvers = {
-	Query: {
-		async getPosts() {
-			try {
-				const posts = await Post.find();
-				return posts;
-			} catch (err) {
-				throw new Error(err);
-			}
-		}
-	}
-};
+const pubsub = new PubSub()
 
 const server = new ApolloServer({
 	typeDefs,
-	resolvers
+	resolvers,
+	context: ({req }) => ({req, pubsub})
 });
 
 mongoose
---
+++ util/validators.js
@@ -0,0 +1,43 @@
+module.exports.validateRegisterInput = (username, email, password, confirmPassword) => {
+	const errors = {};
+	if (username.trim() == '') {
+		errors.username = 'username must not be empty';
+	}
+
+	if (email.trim() == '') {
+		errors.username = 'email must not be empty';
+	} else {
+		const regex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+		if (!email.match(regex)) {
+			errors.email = 'must give a valid email address';
+		}
+
+		if (password === '') {
+			errors.password = 'password must not be empty';
+		} else if (password !== confirmPassword) {
+			errors.confirmPassword = 'passwords must match';
+		}
+
+		return {
+			errors,
+			valid: Object.keys(errors) < 1
+		};
+	}
+};
+
+module.exports.validateLoginInput = (username, password) => {
+	const errors = {};
+
+	if (username.trim() == '') {
+		errors.username = 'username must not be empty';
+	}
+
+	if (password === '') {
+		errors.password = 'password must not be empty';
+	}
+
+	return {
+		errors,
+		valid: Object.keys(errors) < 1
+	};
+};
